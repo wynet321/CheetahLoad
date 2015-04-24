3f3ba00ee6b4dc471cb6217f30e5a94c74d8e4ad
wynet321 <wynet321@163.com>
2015-04-24 17:21:28 +0800

    update swt.jar to 64 bit version.

--- CheetahLoad/src/com/cheetahload/sample/TestSample.java
+++ CheetahLoad/src/com/cheetahload/sample/TestSample.java
@@ -3,13 +3,19 @@ package com.cheetahload.sample;
 import com.cheetahload.TestCase;
 import com.cheetahload.TestConfiguration;
 import com.cheetahload.TestSuite;
+import com.cheetahload.dashboard.Monitor;
 import com.cheetahload.executor.TestLauncher;
 import com.cheetahload.log.Level;
 
 public class TestSample {
 
 	public static void main(String[] args) {
-		TestConfiguration config=TestConfiguration.getTestConfiguration();
+		// test();
+		Monitor monitor = new Monitor();
+	}
+
+	private static void test() {
+		TestConfiguration config = TestConfiguration.getTestConfiguration();
 		config.setDuration(1);
 		config.setVusers(2, "user_", 2, 1);
 		config.setTestSuiteName("Bucket");
