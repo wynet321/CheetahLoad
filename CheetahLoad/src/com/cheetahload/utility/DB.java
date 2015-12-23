0650b9e37fa1af9ea9ff2a09c4997ab3d081b3b7
wynet321 <wynet321@163.com>
2015-12-23 16:01:04 +0800

    fix return in DB class.

--- CheetahLoad/src/com/cheetahload/utility/DB.java
+++ CheetahLoad/src/com/cheetahload/utility/DB.java
@@ -2,12 +2,10 @@ package com.cheetahload.utility;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 import com.cheetahload.TestConfiguration;
 import com.cheetahload.executor.TestLauncher;
@@ -121,6 +119,7 @@ public class DB {
 					.write("DB - insert() - Create SQL statement failed. Please stop current test since performance data can't be recorded correctly. "
 							+ e.getMessage(), Level.ERROR);
 			e.printStackTrace();
+			return false;
 		}
 		int batchCount = 0;
 		String sql = new String();
@@ -134,6 +133,7 @@ public class DB {
 						.write("DB - insert() - Add SQL batch statement failed. Part of performance data can't be recorded. Recommend stop current test."
 								+ e.getMessage(), Level.ERROR);
 				e.printStackTrace();
+				return false;
 			}
 			batchCount++;
 			if (batchCount >= 1000) {
@@ -145,6 +145,17 @@ public class DB {
 							.write("DB - insert() - Execute SQL batch statement failed. Part of performance data can't be recorded. Recommend stop current test. "
 									+ e.getMessage(), Level.ERROR);
 					e.printStackTrace();
+					try {
+						DB.getConnection().rollback();
+					} catch (SQLException e1) {
+						Logger.get(LoggerName.Common).write(
+								"DB - insertBatch() - Roll back failed. Please check DB connection. " + e.getMessage(),
+								Level.ERROR);
+						TestLauncher.stopLogger();
+						e1.printStackTrace();
+						return false;
+					}
+					return false;
 				}
 				batchCount = 0;
 			}
@@ -157,69 +168,6 @@ public class DB {
 					.write("DB - insert() - Execute SQL batch statement failed. Part of performance data can't be recorded. Recommend stop current test."
 							+ e.getMessage(), Level.ERROR);
 			e.printStackTrace();
-		}
-		// catch (SQLException e) {
-		// Logger.get(LoggerName.Common).write(
-		// "DB - insertBatch() - Execute batch SQL failed. Batch SQL: " + sql
-		// + ". Please stop current test since performance data can't be
-		// recorded correctly. "
-		// + e.getMessage(), Level.ERROR);
-		// Logger.get(LoggerName.Common).write("DB - insertBatch() - SQL
-		// parameter values:\n" + logValue,
-		// Level.ERROR);
-		// e.printStackTrace();
-		// try {
-		// DB.getConnection().rollback();
-		// } catch (SQLException e1) {
-		// Logger.get(LoggerName.Common).write(
-		// "DB - insertBatch() - Roll back failed. Please check DB connection. "
-		// + e.getMessage(),
-		// Level.ERROR);
-		// TestLauncher.stopLogger();
-		// e1.printStackTrace();
-		// }
-		// TestLauncher.stopLogger();
-		// return false;
-		// }
-		return true;
-
-	}
-
-	public static boolean insert(String sql, ConcurrentLinkedQueue<String> queue) {
-		if (null == sql || sql.isEmpty()) {
-			Logger.get(LoggerName.Common).write("DB - insert() - SQL string is null or empty.", Level.ERROR);
-			return false;
-		}
-
-		String logValue = new String();
-		try {
-			PreparedStatement sqlStatement = getConnection().prepareStatement(sql);
-			int batchCount = 0;
-			while (!queue.isEmpty()) {
-				logValue = queue.poll();
-				String[] value = logValue.split(",");
-				sqlStatement.setString(1, value[0]);
-				sqlStatement.setString(2, value[1]);
-				sqlStatement.setInt(4, Integer.parseInt(value[3]));
-				sqlStatement.setString(3, value[2]);
-				sqlStatement.addBatch();
-				batchCount++;
-				if (batchCount >= 1000) {
-					sqlStatement.executeBatch();
-					getConnection().commit();
-					batchCount = 0;
-				}
-			}
-			sqlStatement.executeBatch();
-			getConnection().commit();
-		} catch (SQLException e) {
-			Logger.get(LoggerName.Common)
-					.write("DB - insertBatch() - Execute batch SQL failed. Batch SQL: " + sql
-							+ ". Please stop current test since performance data can't be recorded correctly. "
-							+ e.getMessage(), Level.ERROR);
-			Logger.get(LoggerName.Common).write("DB -  insertBatch() - SQL parameter values:\n" + logValue,
-					Level.ERROR);
-			e.printStackTrace();
 			try {
 				DB.getConnection().rollback();
 			} catch (SQLException e1) {
@@ -228,11 +176,68 @@ public class DB {
 						Level.ERROR);
 				TestLauncher.stopLogger();
 				e1.printStackTrace();
+				return false;
 			}
-			TestLauncher.stopLogger();
 			return false;
 		}
+
 		return true;
 
 	}
+
+	// public static boolean insert(String sql, ConcurrentLinkedQueue<String>
+	// queue) {
+	// if (null == sql || sql.isEmpty()) {
+	// Logger.get(LoggerName.Common).write("DB - insert() - SQL string is null
+	// or empty.", Level.ERROR);
+	// return false;
+	// }
+	//
+	// String logValue = new String();
+	// try {
+	// PreparedStatement sqlStatement = getConnection().prepareStatement(sql);
+	// int batchCount = 0;
+	// while (!queue.isEmpty()) {
+	// logValue = queue.poll();
+	// String[] value = logValue.split(",");
+	// sqlStatement.setString(1, value[0]);
+	// sqlStatement.setString(2, value[1]);
+	// sqlStatement.setInt(4, Integer.parseInt(value[3]));
+	// sqlStatement.setString(3, value[2]);
+	// sqlStatement.addBatch();
+	// batchCount++;
+	// if (batchCount >= 1000) {
+	// sqlStatement.executeBatch();
+	// getConnection().commit();
+	// batchCount = 0;
+	// }
+	// }
+	// sqlStatement.executeBatch();
+	// getConnection().commit();
+	// } catch (SQLException e) {
+	// Logger.get(LoggerName.Common)
+	// .write("DB - insertBatch() - Execute batch SQL failed. Batch SQL: " + sql
+	// + ". Please stop current test since performance data can't be recorded
+	// correctly. "
+	// + e.getMessage(), Level.ERROR);
+	// Logger.get(LoggerName.Common).write("DB - insertBatch() - SQL parameter
+	// values:\n" + logValue,
+	// Level.ERROR);
+	// TestLauncher.stopLogger();
+	// e.printStackTrace();
+	// try {
+	// DB.getConnection().rollback();
+	// } catch (SQLException e1) {
+	// Logger.get(LoggerName.Common).write(
+	// "DB - insertBatch() - Roll back failed. Please check DB connection. " +
+	// e.getMessage(),
+	// Level.ERROR);
+	// TestLauncher.stopLogger();
+	// e1.printStackTrace();
+	// return false;
+	// }
+	// return false;
+	// }
+	// return true;
+	// }
 }
