09bfe9531f53e7f20c423c2f6c400013c205acb3
wynet321 <wynet321@163.com>
2016-01-04 11:02:43 +0800

    restruct createTables() from getConnection() method.

--- CheetahLoad/src/com/cheetahload/db/DB.java
+++ CheetahLoad/src/com/cheetahload/db/DB.java
@@ -16,35 +16,51 @@ import com.cheetahload.log.LoggerName;
 public class DB {
 
 	private static Connection connection;
-	private final static String TIMERTABLEDEFINITION = "create table timer (testname varchar(50), scriptname varchar(20), username varchar(20),duration int)";
-	private final static String CONFIGURATIONTABLEDEFINITION = "create table configuration (test_name varchar(50), tester_name varchar(20), tester_mail varchar(50),test_suite_name varchar(20), user_count int, duration int, loops int, think_time int, log_level varchar(10), log_file_size int,log_write_rate int)";
+	private final static String TIMERTABLEDEFINITION = "create table timer (test_name varchar(50), script_name varchar(20), vuser_name varchar(20),duration int)";
+	private final static String CONFIGURATIONTABLEDEFINITION = "create table configuration (test_name varchar(50), tester_name varchar(20), tester_mail varchar(50),test_suite_name varchar(20), vuser_count int, duration int, loops int, think_time int, log_level varchar(10), log_file_size int, log_write_rate int)";
 
 	private static Connection getConnection() {
 		if (connection == null) {
+			StringBuilder connectionString = new StringBuilder();
+			connectionString.append("jdbc:sqlite:").append(TestConfiguration.getTestConfiguration().getLogPath())
+					.append("/").append(TestConfiguration.getTestConfiguration().getTestName()).append(".db");
+
 			try {
 				Class.forName("org.sqlite.JDBC");
-				StringBuilder connectionString = new StringBuilder();
-				connectionString.append("jdbc:sqlite:").append(TestConfiguration.getTestConfiguration().getLogPath())
-						.append("/").append(TestConfiguration.getTestConfiguration().getTestName()).append(".db");
-				connection = DriverManager.getConnection(connectionString.toString());
-				connection.createStatement().executeUpdate(TIMERTABLEDEFINITION);
-				connection.createStatement().executeUpdate(CONFIGURATIONTABLEDEFINITION);
-				connection.setAutoCommit(false);
 			} catch (ClassNotFoundException e) {
 				Logger.get(LoggerName.Common).write(
 						"DB - getConnection() - JDBC class not found. Need to stop current test. " + e.getMessage(),
 						Level.ERROR);
 				e.printStackTrace();
 				TestLauncher.stopLogger();
-			} catch (Exception e) {
-				Logger.get(LoggerName.Common).write(
-						"DB - getConnection() - Failed to create Connection to DB. Need to stop current test. "
-								+ e.getMessage(), Level.ERROR);
+			}
+			try {
+				connection = DriverManager.getConnection(connectionString.toString());
+				connection.setAutoCommit(false);
+			} catch (SQLException e) {
+				Logger.get(LoggerName.Common)
+						.write("DB - getConnection() - Failed to create Connection to DB '"
+								+ connectionString.toString() + "'. Need to stop current test. " + e.getMessage(),
+						Level.ERROR);
 				e.printStackTrace();
 				TestLauncher.stopLogger();
 			}
+			createTables();
 		}
 		return connection;
+	}
+
+	private static void createTables() {
+		try {
+			getConnection().createStatement().executeUpdate(TIMERTABLEDEFINITION);
+			getConnection().createStatement().executeUpdate(CONFIGURATIONTABLEDEFINITION);
+			getConnection().commit();
+		} catch (SQLException e) {
+			Logger.get(LoggerName.Common).write("DB - createTables() - Create DB tables failed. " + e.getMessage(),
+					Level.ERROR);
+			e.printStackTrace();
+			TestLauncher.stopLogger();
+		}
 	}
 
 	public static void closeConnection() {
@@ -53,15 +69,15 @@ public class DB {
 		try {
 			connection.close();
 		} catch (SQLException e) {
-			Logger.get(LoggerName.Common).write(
-					"DB - closeConnection() - Close DB connection failed! " + e.getMessage(), Level.ERROR);
+			Logger.get(LoggerName.Common)
+					.write("DB - closeConnection() - Close DB connection failed! " + e.getMessage(), Level.ERROR);
 			e.printStackTrace();
 			TestLauncher.stopLogger();
 		}
 	}
 
 	public static boolean insert(String sql) {
-		boolean result=false;
+		boolean result = false;
 		if (null == sql || sql.isEmpty()) {
 			Logger.get(LoggerName.Common).write("DB - insert() - SQL string is null or empty.", Level.DEBUG);
 			return result;
@@ -78,7 +94,7 @@ public class DB {
 			return result;
 		}
 		try {
-			result=sqlStatement.execute(sql);
+			result = sqlStatement.execute(sql);
 			getConnection().commit();
 		} catch (SQLException e) {
 			Logger.get(LoggerName.Common)
@@ -93,15 +109,15 @@ public class DB {
 						Level.ERROR);
 				TestLauncher.stopLogger();
 				e1.printStackTrace();
-				result=false;
+				result = false;
 			}
-			result=false;
+			result = false;
 		}
 		return result;
 	}
 
 	public static boolean insert(List<String> list) {
-		boolean result=false;
+		boolean result = false;
 		if (null == list || list.isEmpty()) {
 			Logger.get(LoggerName.Common).write("DB - insert() - SQL list is null or empty.", Level.DEBUG);
 			return result;
@@ -159,7 +175,7 @@ public class DB {
 		try {
 			sqlStatement.executeBatch();
 			getConnection().commit();
-			result=true;
+			result = true;
 		} catch (SQLException e) {
 			Logger.get(LoggerName.Common)
 					.write("DB - insert() - Execute SQL batch statement failed. Part of performance data can't be recorded. Recommend stop current test."
