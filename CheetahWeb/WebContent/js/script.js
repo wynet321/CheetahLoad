bc901b752317c0f63251625bfd4169405d5f3687
Dennis <Dennis@Dennis-PC>
2016-03-01 17:24:53 +0800

    1. Add chart and json support. 2. generate chart by restAPI. left work: debug for showing.

--- CheetahWeb/WebContent/js/script.js
+++ CheetahWeb/WebContent/js/script.js
@@ -1,50 +1,69 @@
-function first() {
+var AxisX = 0;
+var chart;
+var initializeChart = function() {
+	//chart = getChart();
+	generateChart();
+}
+var generateChart = function() {
+	var lineData = {
+		labels : [],
+		datasets : [ {
+			label : "CPU",
+			fillColor : "rgba(220,220,220,0.2)",
+			strokeColor : "rgba(220,220,220,1)",
+			pointColor : "rgba(220,220,220,1)",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "rgba(220,220,220,1)",
+			data : []
+		} ]
+	};
+
 	var cpu = document.getElementById("cpu").getContext("2d");
-	myLine = new Chart(cpu).Line(lineChartData, {
-		responsive : true
+	var myLine = new Chart(cpu).Line(lineData, {
+		responsive : true,
+		bezierCurve : false,
+		pointDot : false,
+		scaleShowGridLines : false
 	});
-	window.setInterval("update()", 5000);
+	var time = window.setInterval("updateChart()", 2000);
+	chart = {
+		cpu : [{ "chart":myLine, "time":time,"test":1} ]
+	};
 }
-var i = 0;
-var myLine;
-var update = function() {
-	i++;
-	myLine.addData([ randomScalingFactor(), randomScalingFactor() ], 6 + i);
-	myLine.update();
 
+var stopUpdate = function(chart) {
+	clearTimeout(chart.cpu[0].time);
 }
-var randomScalingFactor = function() {
-	return Math.round(Math.random() * 100)
-};
 
-var lineChartData = {
-	labels : [ 0, 1, 2, 3, 4, 5, 6 ],
-	datasets : [
-			{
-				label : "My First dataset",
-				fillColor : "rgba(220,220,220,0.2)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(220,220,220,1)",
-				data : [ randomScalingFactor(), randomScalingFactor(),
-						randomScalingFactor(), randomScalingFactor(),
-						randomScalingFactor(), randomScalingFactor(),
-						randomScalingFactor() ]
-			},
-			{
-				label : "My Second dataset",
-				fillColor : "rgba(151,187,205,0.2)",
-				strokeColor : "rgba(151,187,205,1)",
-				pointColor : "rgba(151,187,205,1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(151,187,205,1)",
-				data : [ randomScalingFactor(), randomScalingFactor(),
-						randomScalingFactor(), randomScalingFactor(),
-						randomScalingFactor(), randomScalingFactor(),
-						randomScalingFactor() ]
-			} ]
+var getXHR = function() {
+	var xhttp;
+	if (window.XMLHttpRequest) {
+		xhttp = new XMLHttpRequest();
+	} else {
+		// code for IE6, IE5
+		xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	return xhttp;
+}
+var updateChart = function(chart) {
+	var xhttp = getXHR();
+	xhttp.open("GET", "/CheetahWeb/servlet/getData", true);
+	xhttp.setRequestHeader("If-Modified-Since", "0");
+	xhttp.send();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			var data = xhttp.responseText;
+			var content = JSON.parse(data);
+			var increment = 0;
+			while (content.data[j]) {
+				chart.cpu[0].chart.addData([ content.data[increment] ], AxisX
+						+ increment);
+				increment++;
+			}
+			AxisX += increment;
+			chart.cpu[0].chart.update();
+		}
+	}
 
 }
