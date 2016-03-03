92f35b79acdab214535a1ec7a963ddce0d80f996
Dennis <Dennis@169.254.27.136>
2016-03-03 21:24:51 +0800

    Implemented chart generation.

--- CheetahWeb/WebContent/js/script.js
+++ CheetahWeb/WebContent/js/script.js
@@ -1,69 +1,76 @@
-var AxisX = 0;
-var chart;
-var initializeChart = function() {
-	//chart = getChart();
-	generateChart();
+var cheetahChart=new Object();
+var initializeCheetahChart = function() {
+	generateChart("cpu");
+	generateChart("memory");
+	generateChart("disk");
 }
-var generateChart = function() {
+
+var generateChart=function(name){
 	var lineData = {
-		labels : [],
-		datasets : [ {
-			label : "CPU",
-			fillColor : "rgba(220,220,220,0.2)",
-			strokeColor : "rgba(220,220,220,1)",
-			pointColor : "rgba(220,220,220,1)",
-			pointStrokeColor : "#fff",
-			pointHighlightFill : "#fff",
-			pointHighlightStroke : "rgba(220,220,220,1)",
-			data : []
-		} ]
-	};
+			labels : [],
+			datasets : [ {
+				label : "CPU",
+				fillColor : "rgba(220,220,220,0.2)",
+				strokeColor : "rgba(220,220,220,1)",
+				pointColor : "rgba(220,220,220,1)",
+				pointStrokeColor : "#fff",
+				pointHighlightFill : "#fff",
+				pointHighlightStroke : "rgba(220,220,220,1)",
+				data : []
+			} ]
+		};
 
-	var cpu = document.getElementById("cpu").getContext("2d");
-	var myLine = new Chart(cpu).Line(lineData, {
-		responsive : true,
-		bezierCurve : false,
-		pointDot : false,
-		scaleShowGridLines : false
-	});
-	var time = window.setInterval("updateChart()", 2000);
-	chart = {
-		cpu : [{ "chart":myLine, "time":time,"test":1} ]
-	};
+		var id = document.getElementById(name).getContext("2d");
+		var line = new Chart(id).Line(lineData, {
+			responsive : true,
+			bezierCurve : false,
+			pointDot : false,
+			scaleShowGridLines : false,
+			scaleBeginAtZero : true
+		});
+
+		var timer = window.setInterval("updateChart('"+name+"')", 2000);
+		cheetahChart[name]={"axisX":0,"line":line,"timer":timer};
 }
 
-var stopUpdate = function(chart) {
-	clearTimeout(chart.cpu[0].time);
+var updateChart = function(name) {
+	var xhr = getXHR();
+	xhr.open("GET", "/CheetahWeb/servlet/getData", true);
+	xhr.setRequestHeader("If-Modified-Since", "0");
+	xhr.send();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			var content = JSON.parse(xhr.responseText);
+			var increment = 0;
+			var currentAxisX = cheetahChart[name].axisX;
+			while (content.data[increment]) {
+				cheetahChart[name].line.addData([ content.data[increment] ],
+						currentAxisX + increment);
+				increment++;
+			}
+			cheetahChart[name].axisX += increment;
+			cheetahChart[name].line.update();
+		}
+	}
+}
+
+var stopUpdateChart = function(timer) {
+	clearTimeout(timer);
+}
+
+var stopAllUpdateChart=function(){
+	for(var name in cheetahChart){
+		stopUpdateChart(cheetahChart[name].timer);
+	}
 }
 
 var getXHR = function() {
-	var xhttp;
+	var xhr;
 	if (window.XMLHttpRequest) {
-		xhttp = new XMLHttpRequest();
+		xhr = new XMLHttpRequest();
 	} else {
 		// code for IE6, IE5
-		xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");
 	}
-	return xhttp;
-}
-var updateChart = function(chart) {
-	var xhttp = getXHR();
-	xhttp.open("GET", "/CheetahWeb/servlet/getData", true);
-	xhttp.setRequestHeader("If-Modified-Since", "0");
-	xhttp.send();
-	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState == 4 && xhttp.status == 200) {
-			var data = xhttp.responseText;
-			var content = JSON.parse(data);
-			var increment = 0;
-			while (content.data[j]) {
-				chart.cpu[0].chart.addData([ content.data[increment] ], AxisX
-						+ increment);
-				increment++;
-			}
-			AxisX += increment;
-			chart.cpu[0].chart.update();
-		}
-	}
-
+	return xhr;
 }
